c0ca0c3b1b6c4e986425c52999aa00affa159c18
jto <boudhevil@gmail.com>
2016-10-31 21:31:30 +0100

    move hlist

--- src/main/scala/hlist.scala
+++
@@ -1,8 +0,0 @@
-package test
-
-object hlist {
-  def hnil = HFix[ListF[Nil, ?], INil](Nil)
-  def hcons[X, XS <: Inductive](x: X, xs: XS) = HFix[ListF[X, ?], XS](Cons(x, xs))
-
-  val test = hcons(1, hcons("bar", hnil))
-}
--- src/main/scala/list.scala
+++ src/main/scala/list.scala
@@ -10,3 +10,10 @@ object list {
   def nil[A] = Fix[ListF[A, ?]](Nil)
   def cons[A] = (x:A, xs: List[A]) => Fix[ListF[A, ?]](Cons(x, xs))
 }
+
+object hlist {
+  def hnil = HFix[ListF[Nil, ?], INil](Nil)
+  def hcons[X, XS <: Inductive](x: X, xs: XS) = HFix[ListF[X, ?], XS](Cons(x, xs))
+
+  val test = hcons(1, hcons("bar", hnil))
+}
