7f29934eb648aafb1a225e7de406389a2d34229a
jto <boudhevil@gmail.com>
2016-11-02 23:11:02 +0100

    Add HFix cata

--- build.sbt
+++ build.sbt
@@ -1,6 +1,12 @@
+scalaOrganization in ThisBuild := "org.typelevel"
+
 addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.7.1")
 
 libraryDependencies ++= Seq(
   "com.chuusai" %% "shapeless" % "2.3.2",
   "org.typelevel" %% "cats" % "0.8.0"
 )
+
+scalaVersion := "2.11.8"
+
+scalacOptions += "-Ypartial-unification"
--- src/main/scala/list.scala
+++ src/main/scala/list.scala
@@ -1,6 +1,7 @@
 package test
 
-import shapeless.{ DepFn1, Poly }
+import shapeless.{ DepFn1, Poly, Poly1 }
+import shapeless.PolyDefns._
 import cats.Functor
 
 trait BaseList
@@ -27,16 +28,41 @@ object list {
 
   def cata[A, F[_]](f: F[A] => A)(t: Fix[F])(implicit fc: Functor[F]): A =
     f(fc.map(t.f)(cata[A, F](f)))
-
 }
 
 object hlist {
-  def hnil = HFix[ListF[Nil, ?], INil](Nil)
+  val hnil = HFix[ListF[Nil, ?], INil](Nil)
   def hcons[X, XS <: Inductive](x: X, xs: XS) = HFix[ListF[X, ?], XS](Cons(x, xs))
+
+  trait Cata[HF, L <: Inductive] extends DepFn1[L]
+
+  trait LowPriorityCata {
+    implicit def hfixCata[HF <: Poly, F[_], T <: Inductive, OutC](implicit fc: Functor[F], cata: Cata.Aux[HF, T, OutC], f: Case1[HF, F[OutC]]) =
+      new Cata[HF, HFix[F, T]] {
+        type Out = f.Result
+        def apply(t: HFix[F, T]) =
+          f(fc.map(t.f)(t => cata(t)))
+      }
+  }
+
+  object Cata extends LowPriorityCata{
+    type Aux[HF <: Poly, L <: Inductive, Out0] = Cata[HF, L] { type Out = Out0 }
+
+    def apply[HF <: Poly, L <: Inductive](implicit c: Cata[HF, L]): Aux[HF, L, c.Out] = c
+
+    implicit def lastCata[HF <: Poly, F[_]](implicit fc: Functor[F], f: Case1[HF, F[INil]]): Aux[HF, HFix[F, INil], f.Result] =
+      new Cata[HF, HFix[F, INil]] {
+        type Out = f.Result
+        def apply(t: HFix[F, INil]) = f(t.f)
+      }
+  }
+
+  def cata[HF <: Poly, L <: Inductive](l: L, f: HF)(implicit c: Cata[HF, L]) =
+    c(l)
 }
 
-object test {
-  import list._, hlist._
+object listtest {
+  import list._
   val xs = cons(1, cons(1, nil))
 
   val sumList =
@@ -44,6 +70,24 @@ object test {
       case Nil => 0
       case Cons(x, n) => x + n
     } _
+}
+
+object hlisttest {
+  import hlist._
 
   val hs = hcons(1, hcons("bar", hnil))
+  val xs = hcons(1, hcons(1, hnil))
+
+  object plus extends Poly1 {
+    implicit def caseNil =
+      at[ListF[Nil,INil]] { _ => 0 }
+    implicit def caseInt =
+      at[ListF[Int, Int]] {
+        case Cons(x, n) => x + n
+      }
+  }
+
+  val sum0 = cata(hnil, plus)
+  val sum1 = cata(hcons(1, hnil), plus)
+  val sum2 = cata(xs, plus)
 }
